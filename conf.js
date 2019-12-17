2f0c83c486df9499112d4f8b73250521813477f1
jane haenlein <jane-haenlein_student2019@wilder.school>
2019-12-17 13:50:08 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection(
     {
         host:"localhost",
         user:"root",
-        password:"dzen4jane",
+        password:"xxxxxxx",
         database:"tim_burton"
     }
 );
